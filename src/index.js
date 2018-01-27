ed05b52c7a78b7d642dc1b4b731d10dae6b2c3fd
Ruslan858 <cs171182bra@gmail.com>
2018-01-27 06:06:24 +0200

    second commit

--- src/index.js
+++ src/index.js
@@ -2,4 +2,12 @@ import React from 'react'
 import {render} from 'react-dom'
 import App from './components/App'
 
-render(<App/>, document.getElementById('root'))
+function HelloWorld() {
+    return(
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
+
+render(<HelloWorld/>, document.getElementById('root'));
